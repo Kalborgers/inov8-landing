e5085447b57eb4105d6040578a5f38927ec7935e
GianTee20 <60635471+GianTee20@users.noreply.github.com>
2023-06-30 19:47:38 +0800

    Basic sign up functionality

--- src/sections/Signup.jsx
+++ src/sections/Signup.jsx
@@ -1,6 +1,45 @@
-import React from "react";
+import Script from "next/script";
+import { useState } from "react";
+import { signupDB } from "@/firebase/db";
+import { doc, setDoc, runTransaction } from "firebase/firestore";
 
 export default function Signup() {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+
+    async function signupHandler(e) {
+        try {
+            await runTransaction(signupDB, async (transaction) => {
+                var totalCountRef = doc(signupDB, "SignUps", "TotalCount");
+                //Check for total number of Sign Ups
+                var totalCountDoc = await transaction.get(totalCountRef);
+                if (!totalCountDoc.exists()) {
+                    throw "Document does not exist!";
+                }
+
+                //Increment
+                var id = totalCountDoc.get("num");
+                id++;
+
+                //Add new Sign up document
+                await setDoc(doc(signupDB, "SignUps", id.toString()), {
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email
+                });
+
+                //Update total number of sign ups
+                transaction.update(totalCountRef, {
+                    num: id
+                });
+            });
+            console.log("Successfully Signed Up!");
+        } catch (e) {
+            console.log("Sign Up Failed: ", e);
+        }
+    }
+
     return (
         <section id="signup" className="flex flex-col w-full text-[#414046]">
             <div className="m-auto text-center space-y-8 my-32">
@@ -22,30 +61,42 @@ export default function Signup() {
                     </div>
                     <div className="flex flex-col justify-center items-center bg-white rounded-xl align-middle p-24 space-y-4">
                         <div className="flex flex-col">
-                            <label for="firstName">First Name:</label>
+                            <label htmlFor="firstName">First Name:</label>
                             <input
                                 type="text"
+                                id="firstName"
                                 name="firstName"
                                 className="border"
+                                value={firstName}
+                                onChange={(e) => setFirstName(e.target.value)}
                             ></input>
                         </div>
                         <div className="flex flex-col">
-                            <label for="lastName">Last Name:</label>
+                            <label htmlFor="lastName">Last Name:</label>
                             <input
                                 type="text"
+                                id="lastName"
                                 name="lastName"
                                 className="border"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
                             ></input>
                         </div>
                         <div className="flex flex-col">
-                            <label for="email">Email:</label>
+                            <label htmlFor="email">Email:</label>
                             <input
                                 type="text"
+                                id="email"
                                 name="email"
                                 className="border"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                             ></input>
                         </div>
-                        <button className="bg-[#f3df0f] rounded-md px-8 py-2 text-xl font-bold">
+                        <button
+                            className="bg-[#f3df0f] rounded-md px-8 py-2 text-xl font-bold"
+                            onClick={signupHandler}
+                        >
                             SIGN UP
                         </button>
                     </div>
